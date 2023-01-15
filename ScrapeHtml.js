7e44e9c42a5bc3fc35898776f7eb982ccf49b13e
Jager <96440779+hassan6846@users.noreply.github.com>
2023-01-15 15:24:19 +0500

    main

---
+++ ScrapeHtml.js
@@ -0,0 +1,4 @@
+const puppet = require('puppeteer');//requiring main module
+const fs = require('fs');//node file system
+const path = require('path');// node path module similar to python os.path
+// writing file
